4841d421f50a3b71056d03cf435812cac63dea5c
ZhmakaRoman <shmaka741@gmail.com>
2023-08-15 18:05:39 +0200

    refactor: changed its name

--- Assets/Scripts/DragController.cs
+++ Assets/Scripts/DragController.cs
@@ -1,104 +1,34 @@
-using System;
-using DefaultNamespace;
 using UnityEngine;
-using DG.Tweening;
 
-public class DragController : MonoBehaviour
+
+public class OutlineController : MonoBehaviour
 {
-    [SerializeField] private LayerMask _raycastLayerMask;
-    [SerializeField] private float _movementDuration = 1f;
-    [SerializeField] private Vector3 _offset;
-    private Collider _draggedObject;
-    private Vector3 _startPositionObject;
-    private Vector3 _dropSpotPosition;
-    private Camera _camera;
-    private bool _isOverDropSpot;
+    [SerializeField] private int _highlightIntensity = 4;
+
+    private Outline _outline;
 
     private void Awake()
     {
-        _camera = Camera.main;
+        _outline = GetComponent<Outline>();
     }
 
-
-    private void Update()
+    private void OnMouseDown()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            TakeObject();
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            DragObject();
-        }
-
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            DropObject();
-        }
+        SetFocus();
     }
 
-    private void TakeObject()
+    private void OnMouseUp()
     {
-        var ray = _camera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out var hit))
-        {
-            if (hit.collider.CompareTag("Player"))
-            {
-                _draggedObject = hit.collider;
-                _startPositionObject = _draggedObject.transform.position;
-            }
-        }
+        RemoveFocus();
     }
 
-    private void DragObject()
+    private void SetFocus()
     {
-        var ray = _camera.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out var hit, Mathf.Infinity, _raycastLayerMask))
-        {
-            if (_draggedObject != null)
-            {
-                _draggedObject.transform.position = new Vector3(hit.point.x, hit.point.y, hit.point.z) + _offset;
-
-                CheckDropSpot();
-            }
-        }
+        _outline.OutlineWidth = _highlightIntensity;
     }
 
-    private void DropObject()
+    private void RemoveFocus()
     {
-        if (_draggedObject == null)
-        {
-            return;
-        }
-
-        if (_isOverDropSpot)
-        {
-            _draggedObject.transform.position = _dropSpotPosition + _offset;
-        }
-        else
-        {
-            _draggedObject.transform.DOMove(_startPositionObject, _movementDuration);
-        }
-
-
-        _draggedObject = null;
-    }
-
-    private void CheckDropSpot()
-    {
-        var ray = new Ray(_draggedObject.transform.position, Vector3.down);
-        if (Physics.Raycast(ray, out var hitInfo))
-        {
-            var hit = hitInfo.collider.gameObject;
-            var spot = hit.CompareTag("Finish");
-            if (spot)
-            {
-                _isOverDropSpot = true;
-                _dropSpotPosition = hit.transform.position;
-            }
-        }
+        _outline.OutlineWidth = 0;
     }
 }
