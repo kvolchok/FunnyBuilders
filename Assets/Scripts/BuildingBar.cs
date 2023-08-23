84b85e3844d0c34b164369a7a8820f757963ac88
artyom95 <92054262+artyom95@users.noreply.github.com>
2023-08-23 23:28:44 +0300

    fix: fix buildingBar bug

--- Assets/Scripts/BuildingBar.cs
+++ Assets/Scripts/BuildingBar.cs
@@ -5,40 +5,17 @@ using UnityEngine.UI;
 public class BuildingBar : MonoBehaviour
 {
     [SerializeField] private Slider _buildingBar;
-    [SerializeField] private float _durationFillBuildingBar;
     private float _maxValueBar;
     private Coroutine _fillBuildingBar;
-    public void Initialize(float maxValueBar)
+    
+    public void SetValueBar(float value, float maxValueBar)
     {
-        _maxValueBar = maxValueBar;
-    }
-    public void SetValueBar(float value)
-    {
-        if (_fillBuildingBar != null)
-        {
-            StopCoroutine(_fillBuildingBar);
-        }
-        var currentBuildingBarValue = value / _maxValueBar;
-        _fillBuildingBar = StartCoroutine(SetBuildingBar(currentBuildingBarValue));
+    
+     var settingValue = value ;
+     _buildingBar.value = settingValue ;
 
     }
 
-    private IEnumerator SetBuildingBar (float currentBuildingBarValue)
-    {
-        var currentTime = 0f;
-
-        while (currentTime < _durationFillBuildingBar)
-        {
-            var progress = currentTime / _durationFillBuildingBar;
-
-            var startBuildingBarValue =  _buildingBar.value;
-            var finishBuildingBarValue = currentBuildingBarValue;
-            var currentValue = Mathf.Lerp(startBuildingBarValue, finishBuildingBarValue, progress);
-            _buildingBar.value = currentValue;
-            currentTime += Time.deltaTime;
-            yield return null;
-        }
-        
-    }
+   
    
 }
