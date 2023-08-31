589b5f48887c9992376ff460bf0f9e6e5955fc29
ZhmakaRoman <shmaka741@gmail.com>
2023-08-31 17:53:58 +0200

    feat: added popupText

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,17 +1,19 @@
+using DefaultNamespace;
 using UnityEngine;
+
 
 public class Unit : MonoBehaviour, IDraggable
 {
     private static readonly int _idle = Animator.StringToHash("Idle");
     private static readonly int _work = Animator.StringToHash("Work");
     private static readonly int _run = Animator.StringToHash("Run");
-    
+
     public UnitState State { get; private set; }
     public int Level { get; private set; }
     public int Salary { get; private set; }
 
-    [SerializeField]
-    private SkinnedMeshRenderer _renderer;
+    [SerializeField] private SkinnedMeshRenderer _renderer;
+    [SerializeField] private PopupTextOverWorker _popupText;
 
     private Animator _animator;
 
@@ -19,42 +21,43 @@ public class Unit : MonoBehaviour, IDraggable
     {
         _animator = GetComponent<Animator>();
     }
-
     public void Initialize(UnitSettings unitSettings)
     {
         Level = unitSettings.Level;
         Salary = unitSettings.Salary;
         _renderer.material.color = unitSettings.Color;
-    }
 
+    }
     public void Drag(Vector3 targetPosition)
     {
+
         transform.position = targetPosition;
     }
-
     public Vector3 GetPosition()
     {
         return transform.position;
     }
-    
+
     public void ChangeState(UnitState state)
     {
+        _popupText.HideFloatingText();
         State = state;
 
         switch (State)
         {
             case UnitState.Idle:
                 ShowAnimation(_idle);
+
                 break;
             case UnitState.Run:
                 ShowAnimation(_run);
                 break;
             case UnitState.Work:
                 ShowAnimation(_work);
+                _popupText.ShowFloatingText(Salary);
                 break;
         }
     }
-
     private void ShowAnimation(int state)
     {
         _animator.SetTrigger(state);
